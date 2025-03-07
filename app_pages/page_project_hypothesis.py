38f7554c4e5affee0ab8bc1be27306ffceec5e4c
Alexander <166539943+AlexanderAberg@users.noreply.github.com>
2025-03-07 22:25:01 +0000

    page_project_hypothesis.py updated what we believe and the result

--- app_pages/page_project_hypothesis.py
+++ app_pages/page_project_hypothesis.py
@@ -5,12 +5,12 @@ def page_project_hypothesis_body():
 
     st.write("### Project Hypothesis and Validation")
 
-    # conclusions taken from "02 - Churned Customer Study" notebook
     st.success(
-        f"* We suspect customers are churning with low tenure levels: Correct. "
-        f"The correlation study at Churned Customer Study supports that. \n\n"
+        f"* We believe that the size of the house and its features affect the SalePrice "
+        f" We notice that it does, but it is not the most important feature, because quality has a higher affection and YearBuilt has a bigger importance than a few size related features \n\n"
 
-        f"* A customer survey showed our customers appreciate fibre Optic. "
-        f"A churned user typically has Fibre Optic, as demonstrated by a Churned Customer Study. "
-        f"This insight will be used by the survey team for further discussions and investigations."
+        f"* Our conclusion is that you have to take care of your house to get a good SalePrice "
+        f"This means that a big house with a big garage, porch, yard etc. will get a very high SalePrice if you are also keeping the house in great condition"
+        f"We think that one of the reason YearBuilt has a big affect because a new house is often in better condition, that can also be a reason why a house from 1940 has about the same SalePrice as one from 1960"
+        f"The YearRemodAdd also have a decently high correlation, but much less because a variety of the quality of the house work, while a new house has to a big degree professionals building the house"
     )
