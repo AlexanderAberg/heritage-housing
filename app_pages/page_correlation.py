ebac1ceb9259d8054fd6b446df39055e3255a557
Alexander <166539943+AlexanderAberg@users.noreply.github.com>
2025-03-08 13:23:28 +0000

    Changed and added code in page_correlation.py to fit with new src

--- app_pages/page_correlation.py
+++ app_pages/page_correlation.py
@@ -4,16 +4,18 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 import ppscore as pps
+from src.data_management import load_housing_data
 
 model_path = f"outputs/ml_pipeline/predict_price/v1/regression_pipeline.pkl"
 
 def page_correlation_body():
 
+    df = load_housing_data()
+
     # List of variables to study in the analysis
     vars_to_study = [
         {'1stFlrSF',
         'BsmtFinSF1', 'GarageArea', 'GarageYrBlt',
-        'GrLivArea', 'LotArea', 'MasVnrArea',
         'OpenPorchSF', 'OverallQual', 'TotalBsmtSF',
         'YearBuilt', 'YearRemodAdd'}]
 
@@ -32,51 +34,90 @@ def page_correlation_body():
         ""
     )
 
+    # Subset of data for analysis (target variable and selected features)
+    target_var = 'SalePrice'
+    st.write("#### Data Visualizations")
+
+    # Display histogram of the target variable's distribution
+    if st.checkbox(" Distribution of Target Variable"):
+        st.write("")
+        plot_target_hist(df, target_var)
+
+    # Display correlation and PPS (Predictive Power Score) heatmaps
+    if st.checkbox("Correlation and PPS Heatmap"):
+        df_corr_pearson, df_corr_spearman, pps_matrix = (
+            CalculateCorrAndPPS(df))
+        DisplayCorrAndPPS(df_corr_pearson, df_corr_spearman, pps_matrix,
+                          CorrThreshold=0.3, PPS_Threshold=0.2)
+
     # Function to display scatter plots or categorical distribution plots
-    def scatter_plot_for_eda(col, vars_to_study):
-        """
-        Function to create a scatter plot between a feature and the target
-        variable.
-        """
-        fig = px.scatter(x=col, y=vars_to_study,
-                         title=f"Scatter Plot of {col} vs {vars_to_study}",
-                         trendline="ols", trendline_color_override="red")
+    def scatter_plot_for_eda(df, col, target_var):
+       
+        fig = px.scatter(df, x=col, y=target_var,
+                         title=f"Scatter Plot of {col} vs {target_var}",
+                         trendline="ols", trendline_color_override="orange")
         st.plotly_chart(fig)
 
-    def plot_categorical(col, vars_to_study):
+    def plot_categorical(df, col, target_var):
         """
         Function to create a stacked histogram for categorical variables vs
         target.
         """
-        fig = px.histogram(x=col, color=vars_to_study,
-                           title=f"Distribution of {col} vs {vars_to_study}",
+        fig = px.histogram(df, x=col, color=target_var,
+                           title=f"Distribution of {col} vs {target_var}",
                            barmode='stack')
         fig.update_layout(xaxis_tickangle=-45)
         st.plotly_chart(fig)
 
-    def variables_plots():
-        """
-        Function to plot either scatter plots or categorical plots for all
-        selected variables.
-        """
-        vars_to_study = 'SalePrice'
-      
+    def variables_plots(df_eda):
+    
+        target_var = 'SalePrice'
+        # Iterate over all variables and plot according to type
+        for col in df_eda.drop([target_var], axis=1).columns.to_list():
+            if df_eda[col].dtype == 'object':
+                plot_categorical(df_eda, col, target_var)
+            else:
+                scatter_plot_for_eda(df_eda, col, target_var)
+
     # Display visual analysis for each selected variable
-    if st.checkbox(" Variables Plots - Visual Analysis"):
-        variables_plots()
+    if st.checkbox(" Visual Analysis"):
+        variables_plots(df)
 
 
-def heatmap_pps(df, threshold, figsize=(20, 12), font_annot=8):
+def plot_target_hist(df, target_var):
+  
+    fig = px.histogram(df, x=target_var, marginal="box", nbins=50,
+                       title=f"Distribution of {target_var}")
+    st.plotly_chart(fig)
+
+
+def heatmap_corr(df, threshold, title, figsize=(22, 13), font_annot=9):
+
+    if len(df.columns) > 1:
+        # Create a mask to hide upper triangle and correlations
+        mask = np.zeros_like(df, dtype=bool)
+        mask[np.triu_indices_from(mask)] = True
+        mask[abs(df) < threshold] = True
+
+        # Apply mask to the correlation matrix
+        df_masked = df.mask(mask)
+
+        # Plot heatmap using Plotly
+        fig = px.imshow(
+            df_masked,
+            title=title,
+            color_continuous_scale='plasma',
+            labels={'x': 'Features', 'y': 'Features'},
+            text_auto=True
+        )
+        st.plotly_chart(fig)
+
+
+def heatmap_pps(df, threshold, figsize=(22, 13), font_annot=9):
     """
-    Function to generate a Predictive Power Score (PPS) heatmap.
-    Values below the specified threshold are hidden in the plot.
-
-    Args:
-        df: DataFrame containing the PPS matrix.
-        threshold: PPS value below which cells will be hidden.
-        figsize: Optional tuple to set the size of the heatmap.
-        font_annot: Optional font size for annotations in the heatmap.
+    Generates the Predictive Power Score heatmap.
     """
+
     if len(df.columns) > 1:
         # Create a mask to hide PPS below the threshold
         mask = np.zeros_like(df, dtype=bool)
@@ -88,8 +129,8 @@ def heatmap_pps(df, threshold, figsize=(20, 12), font_annot=8):
         # Plot heatmap using Plotly
         fig = px.imshow(
             df_masked,
-            title="Predictive Power Score Heatmap",
-            color_continuous_scale='viridis',
+            title="Predictive Power Score (PPS) Heatmap",
+            color_continuous_scale='plasma',
             labels={'x': 'Features', 'y': 'Features'},
             text_auto=True
         )
@@ -98,18 +139,10 @@ def heatmap_pps(df, threshold, figsize=(20, 12), font_annot=8):
 
 def CalculateCorrAndPPS(df):
     """
-    Function to calculate both the Pearson and Spearman correlations as well as
-    the Predictive Power Score (PPS) matrix for the dataset.
-
-    Args:
-        df: DataFrame containing the features to analyze.
-
-    Returns:
-        df_corr_pearson: Pearson correlation matrix.
-        df_corr_spearman: Spearman correlation matrix.
-        pps_matrix: Matrix of PPS values between the features.
+    Function to calculate the Spearman and Pearson correlations and
+    the Predictive Power Score matrix for the dataset.
     """
-    # Calculate correlation matrices
+    # Calculate Spearman and Pearson
     df_corr_spearman = df.corr(method="spearman")
     df_corr_spearman.name = 'corr_spearman'
     df_corr_pearson = df.corr(method="pearson")
@@ -130,19 +163,21 @@ def CalculateCorrAndPPS(df):
 
 def DisplayCorrAndPPS(df_corr_pearson, df_corr_spearman, pps_matrix,
                       CorrThreshold, PPS_Threshold,
-                      figsize=(20, 12), font_annot=8):
+                      figsize=(22, 13), font_annot=9):
     """
-    Function to display both the correlation heatmap and the PPS heatmap.
+    Function to display both the Spearman and Pearson correlation heatmap
+    and the PPS heatmap.
+    """
+    # Display Pearson correlation heatmap
+    heatmap_corr(df=df_corr_pearson, threshold=CorrThreshold,
+                 title="Pearson Correlation Heatmap", figsize=figsize,
+                       font_annot=font_annot)
 
-    Args:
-        df_corr_pearson: Pearson correlation matrix.
-        df_corr_spearman: Spearman correlation matrix.
-        pps_matrix: Matrix of PPS values between the features.
-        CorrThreshold: Threshold to mask correlations in the heatmap.
-        PPS_Threshold: Threshold to mask PPS values in the heatmap.
-        figsize: Optional tuple for setting the size of the heatmaps.
-        font_annot: Optional font size for annotations in the heatmap.
-    """
+    # Display Spearman correlation heatmap
+    heatmap_corr(df=df_corr_spearman, threshold=CorrThreshold,
+                 title="Spearman Correlation Heatmap", figsize=figsize,
+                 font_annot=font_annot)
+
     # Display PPS heatmap
     heatmap_pps(df=pps_matrix, threshold=PPS_Threshold, figsize=figsize,
                 font_annot=font_annot)
