cef096a658c3604d0a1d96dc98a94fdfac896764
Alexander <166539943+AlexanderAberg@users.noreply.github.com>
2025-03-08 19:25:23 +0000

    Updated the page_machine_learning identation and for it to get info from src

--- app_pages/page_machine_learning.py
+++ app_pages/page_machine_learning.py
@@ -3,17 +3,13 @@ import pandas as pd
 import joblib
 import os
 import matplotlib.pyplot as plt
-
+from src.data_management import load_ames_data, load_pkl_file
+from src.machine_learning.evaluate_reg import regression_performance
 
 def load_pkl_file(file_path):
     """
     Loads a pickled file (model) using joblib from the given file path.
 
-    Parameters:
-    file_path (str): The path to load the pickled file (model).
-
-    Returns:
-    model: The machine learning model.
     """
     try:
         # Attempt to load the pickled file using joblib
@@ -27,7 +23,7 @@ def load_pkl_file(file_path):
 
 def page_machine_learning_body():
     """
-    IS displaying the machine learning pipeline and regression
+    Is displaying the machine learning pipeline and regression
     performance plots.
     """
     # Define the version of the pipeline being used
@@ -36,6 +32,10 @@ def page_machine_learning_body():
     # Load the regression pipeline (model)
     price_pipe = load_pkl_file(
         f"outputs/ml_pipeline/predict_price/{version}/regression_pipeline.pkl"
+    )
+
+    price_feat_importance = plt.imread(
+        f"outputs/ml_pipeline/predict_price/{version}/features_importance.png"
     )
 
     # Load the training and test datasets (features and targets)
@@ -57,11 +57,11 @@ def page_machine_learning_body():
 
     # Show an introductory message about the ML pipeline
     st.info(
-        "* To answer the Business Requirement 2, a Regressor model was trained and the pipeline "
-        "tuned aiming for at least 0.9 accuracy in predicting the sales "
-        "price of a property with a set of attributes.\n"
+        "* To answer the Business Requirement 2, a Regressor model was trained" 
+        " and the pipeline tuned aiming for at least 0.9 accuracy in "
+        "predicting the sales price of a property with a set of attributes.\n"
         "* The pipeline performance for the best model on the train and test "
-        "set is R2 = 1.0 (flawless score) and R2 = 0.883 (almost 0.9) respectively.\n"
+        "set is R2 = 1.0 (flawless) and R2 =0.883 (almost 0.9) respectively.\n"
         "* We present the pipeline steps, best features list along with "
         "feature importance plot, pipeline performance and regression "
         "performance report below."
@@ -73,24 +73,24 @@ def page_machine_learning_body():
     st.code(price_pipe)  # Display the code of the pipeline
     st.write("---")
 
-    # Show the features the model was trained on and their importance
     st.write("* The features the model was trained and their importance")
-    st.write(X_train.columns.to_list())  # List of features used for training
+    st.write(X_train.columns.to_list())  # The features used for training
     st.write("---")
 
-    # Display information about the pipeline performance goals
+    # Information about performance goals for the pipeline
     st.write("### Pipeline Performance")
     st.write("##### Performance goal of the predictions:")
     st.write(
-        "* We agreed with the client an R2 average score of at least 0.9 on the "
-        "train set and the test set."
+        "* We agreed with the client an R2 average score of at least 0.9 on "
+        "the train set and the test set."
     )
     st.write(
-        "* Our Machine Learning pipeline performance shows that our model performance "
-        "metrics have achieved the requirements, thanks to the flawless score on the train model."
+        "* Our Machine Learning pipeline performance shows that our "
+        "model performance metrics have achieved the requirements, "
+        "thanks to the flawless score on the train model."
     )
 
-    # Display the regression performance plots
+    # The regression performance plots
     st.write("### Regression Performance Plots")
     st.write(
         "* The regression performance plots below indicate that the model "
@@ -99,7 +99,7 @@ def page_machine_learning_body():
         "dependable."
     )
 
-    # Display the regression performance plot
+    # The regression performance plot
     image_path = 'docs/plots/regression_performance.png'
     st.image(image_path, caption="Regression Performance",
              use_container_width=True)
