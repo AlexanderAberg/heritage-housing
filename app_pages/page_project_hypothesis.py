bda77fa77e1538f1f89c6f4ac0c191669983b04d
Alexander <166539943+AlexanderAberg@users.noreply.github.com>
2025-03-08 13:56:00 +0000

    Updated page_project_hypothesis.py

--- app_pages/page_project_hypothesis.py
+++ app_pages/page_project_hypothesis.py
@@ -6,11 +6,22 @@ def page_project_hypothesis_body():
     st.write("### Project Hypothesis and Validation")
 
     st.success(
-        f"* We believe that the size of the house and its features affect the SalePrice "
-        f" We notice that it does, but it is not the most important feature, because quality has a higher affection and YearBuilt has a bigger importance than a few size related features \n\n"
+        "* We believe that the size of the house and its features affect the "
+        "SalePrice "
+        "We notice that it does, but it is not the most important feature, "
+        "because quality has a higher affection and YearBuilt has "
+        "a bigger importance than a few size related features \n\n"
 
-        f"* Our conclusion is that you have to take care of your house to get a good SalePrice "
-        f"This means that a big house with a big garage, porch, yard etc. will get a very high SalePrice if you are also keeping the house in great condition"
-        f"We think that one of the reason YearBuilt has a big affect because a new house is often in better condition, that can also be a reason why a house from 1940 has about the same SalePrice as one from 1960"
-        f"The YearRemodAdd also have a decently high correlation, but much less because a variety of the quality of the house work, while a new house has to a big degree professionals building the house"
+        "* Our conclusion is that you have to take care of your "
+        "to get a good SalePrice "
+        "This means that a big house with a big garage, porch, yard etc. "
+        "will get a very high SalePrice if you are also keeping the house in "
+        "great condition \n\n"
+        "We think that one of the reason YearBuilt has a big affect because a "
+        "new house is often in better condition,"
+        "that can also be a reason why a house from 1940 "
+        "has about the same SalePrice as one from 1960"
+        "The YearRemodAdd also have a decently high correlation, but "
+        "much less because a variety of the quality of the house work, while "
+        "a new house has to a big degree professionals building the house"
     )
