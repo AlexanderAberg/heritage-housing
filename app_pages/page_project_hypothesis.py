2c7d0cbba65ff32fc2700c41f521b1db841b3861
Alexander <166539943+AlexanderAberg@users.noreply.github.com>
2025-03-08 14:11:14 +0000

    Added periods in page_project_hypothesis.py

--- app_pages/page_project_hypothesis.py
+++ app_pages/page_project_hypothesis.py
@@ -7,22 +7,23 @@ def page_project_hypothesis_body():
 
     st.success(
         "* We believe that the size of the house and its features affect the "
-        "SalePrice "
+        "SalePrice. "
         "We notice that it does, but it is not the most important feature, "
         "because quality has a higher effect and YearBuilt has "
-        "a bigger importance than a few size related features \n\n"
+        "a bigger importance than a few size related features. \n\n"
 
         "* Our conclusion is that you have to take care of your "
-        "to get a good SalePrice "
+        "to get a good SalePrice. "
         "This means that a big house with a big garage, porch, yard etc. "
         "will get a very high SalePrice if you are also keeping the house in "
-        "great condition \n\n"
+        "great condition. \n\n"
 
         "* We think that one of the reasons YearBuilt has a big effect because"
         " a new house is often in better condition, "
         "that can also be a reason why a house from 1940 "
-        "has about the same SalePrice as one from 1960 "
+        "has about the same SalePrice as one from 1960. "
         "The YearRemodAdd also has a decently high correlation, but "
         "much less because a variety of the quality of the house work, while "
-        "a new house has to a big degree professionals building the house \n\n"
+        "a new house has to a big degree professionals building the house. "
+        "\n\n"
     )
